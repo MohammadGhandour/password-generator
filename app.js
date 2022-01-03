877ade4b4b25fdcc929da840f796aeef1b8726f4
MohammadGhandour <mohammadghandour02@outlook.com>
2022-01-03 21:25:43 +0400

    Ajout des fichiers du password generator

---
+++ app.js
@@ -0,0 +1,34 @@
+const passwordOutput = document.getElementById('password-output');
+const dataLowerCase = "qwertyuioplkjhgfdsazxcvbnm".split('');
+const dataUpperCase = "QWERTYUIOPLKJHGFDSAZXCVBNM".split('');
+const dataNumbers = "0123456789".split('');
+const dataSymbols = "!@#$%^&*-_=+\|:;',.>/?~".split('');
+const displayPasswordLength = document.getElementById('display-password-length');
+
+function generatePassword() {
+    const data = [].concat(
+        lowerCase.checked ? dataLowerCase : [],
+        upperCase.checked ? dataUpperCase : [],
+        numbers.checked ? dataNumbers : [],
+        symbols.checked ? dataSymbols : []
+    );
+    
+    let passwordLength = parseInt(displayPasswordLength.value);
+
+        let newPassword = ''
+
+    if(data.length == 0){
+        alert ('Veuillez sélectionner au moins une critère');
+        return;
+    }
+    
+    for (let i = 0; i < passwordLength; i++) {
+        newPassword += data[Math.floor(Math.random() * data.length)];
+    }
+    passwordOutput.value = newPassword;
+    passwordOutput.style.fontFamily = 'serif';
+    passwordOutput.select();
+    document.execCommand('copy');
+    generateButton.innerHTML = "Copied !";
+    setTimeout(() => {generateButton.innerHTML = "Generate Password"}, 2000);
+}
